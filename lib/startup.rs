ba53b9856571fdb10799a7905a0a617b19d74a6d
John Jekel <JZJisawesome123@gmail.com>
2023-11-29 13:00:45 -0500

    Proper thread safety, looks like the dpi/pli/vpi aren't thread safe

--- lib/startup.rs
+++ lib/startup.rs
@@ -23,6 +23,12 @@
  * Uses
  * --------------------------------------------------------------------------------------------- */
 
+use std::thread::{ThreadId};
+
+/* ------------------------------------------------------------------------------------------------
+ * Macros
+ * --------------------------------------------------------------------------------------------- */
+
 #[macro_export]
 macro_rules! vlog_startup_routines {
     //($($arg:tt),*) => {
@@ -30,6 +36,10 @@ macro_rules! vlog_startup_routines {
         #[doc(hidden)]
         mod ___sv_api_vlog_startup_routines___ {
             extern "C" fn ___sv_api_call_vlog_startup_routines___() {
+                //SAFETY: This is the only place we allow this function to be called without
+                //warning that the caller would violate safety by doing so.
+                unsafe { ::sv_api::startup::___startup_routines_started___(); }
+
                 $(
                     super::$arg();
                 )*
@@ -56,38 +66,47 @@ macro_rules! vlog_startup_routines {
     };
 }
 
-/* ------------------------------------------------------------------------------------------------
- * Macros
- * --------------------------------------------------------------------------------------------- */
+macro_rules! this_fn_str {
+    () => {{
+        //Thanks https://stackoverflow.com/questions/38088067/equivalent-of-func-or-function-in-rust
+        fn ___subfunction___() {}
+        fn ___type_name_of___<T>(_: T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        ___type_name_of___(___subfunction___)
+            .strip_suffix("::___subfunction___")
+            .expect("Suffix should exist!")
+    }}
+}
+pub(crate) use this_fn_str;
 
 macro_rules! panic_if_in_startup_routine {
-    () => {
+    () => {{
         if crate::startup::in_startup_routine() {
-            //Thanks https://stackoverflow.com/questions/38088067/equivalent-of-func-or-function-in-rust
-            fn ___dummy___() {}
-            fn ___type_name_of___<T>(_: T) -> &'static str {
-                std::any::type_name::<T>()
-            }
-            let ___fn_name___ = ___type_name_of___(___dummy___)
-                .strip_suffix("::___dummy___")
-                .expect("Suffix should exist!");
-            panic!("{}() cannot be called during a startup routine!", ___fn_name___);
+            panic!("{}() cannot be called during a startup routine!", crate::startup::this_fn_str!());
         }
-    };
+    }};
 }
 pub(crate) use panic_if_in_startup_routine;
 
-/* ------------------------------------------------------------------------------------------------
- * Constants
- * --------------------------------------------------------------------------------------------- */
-
-//TODO
+macro_rules! panic_if_not_main_thread {
+    () => {{
+        if !crate::startup::is_main_thread() {
+            panic!("{}() cannot be called by any thread other than the main thread!", crate::startup::this_fn_str!());
+        }
+    }};
+}
+pub(crate) use panic_if_not_main_thread;
 
 /* ------------------------------------------------------------------------------------------------
  * Static Variables
  * --------------------------------------------------------------------------------------------- */
 
+///Used to uphold invariants the C interface requires
 static INIT_FINISHED: std::sync::OnceLock<()> = std::sync::OnceLock::new();
+
+///Used to ensure thread safety since the C interface is not thread safe
+static MAIN_THREAD_ID: std::sync::OnceLock<ThreadId> = std::sync::OnceLock::new();
 
 /* ------------------------------------------------------------------------------------------------
  * Types
@@ -117,14 +136,35 @@ static INIT_FINISHED: std::sync::OnceLock<()> = std::sync::OnceLock::new();
  * Functions
  * --------------------------------------------------------------------------------------------- */
 
-//Should only be called by the vlog_startup_routines! macro, any other use is undefined behaviour
+///Should only be called by the vlog_startup_routines! macro, any other use is undefined behaviour
+#[doc(hidden)]
+pub unsafe fn ___startup_routines_started___() {
+    let this_thread = std::thread::current();
+    let this_thread_id = this_thread.id();
+    MAIN_THREAD_ID.set(this_thread_id).expect("___startup_routines_started___() was called manually at some point!");
+}
+
+///Should only be called by the vlog_startup_routines! macro, any other use is undefined behaviour
 #[doc(hidden)]
 pub unsafe fn ___startup_routines_finished___() {
     INIT_FINISHED.set(()).expect("___startup_routines_finished___() was called manually at some point!");
 }
 
+///Returns true if we are currently in a startup routine (and thus most SV interfaces are unavailable)
 pub fn in_startup_routine() -> bool {
+    debug_assert!(is_main_thread());
     INIT_FINISHED.get().is_none()
+}
+
+///Returns true if we are currently in the main thread
+///
+///Useful to double-check you aren't in another thread (otherwise no SV interfaces would be
+///available)
+pub fn is_main_thread() -> bool {
+    let this_thread = std::thread::current();
+    let this_thread_id = this_thread.id();
+    let main_thread_id = MAIN_THREAD_ID.get().expect("We should already know the main thread id");
+    this_thread_id == *main_thread_id
 }
 
 /* ------------------------------------------------------------------------------------------------
